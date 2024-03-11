d9e05cbdea3e9a4f2ac1c56e8d702aca4146605a
doanvalerie <valeriephuongdoan@gmail.com>
2024-03-11 14:32:29 -0700

    Fix: make sign up form take up remaining vh

--- src/app/(pages)/sign-up/page.tsx
+++ src/app/(pages)/sign-up/page.tsx
@@ -3,9 +3,11 @@ import Form from './_components/Form';
 
 export default function SignUp() {
   return (
-    <div className="flex grow flex-col items-center justify-center gap-lg p-lg lg:flex-row">
-      <Description />
-      <Form />
+    <div>
+      <div className="flex h-[calc(100vh-theme(spacing.navbar))] min-h-fit grow flex-col items-center justify-center gap-lg p-lg lg:flex-row">
+        <Description />
+        <Form />
+      </div>
     </div>
   );
 }
